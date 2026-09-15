54ecd92ced9a742c35541684ffa25d701ffedd94
foresightplanner <foresightplanner@gmail.com>
2026-09-15 14:13:50 -0500

    Optimized image and video loading

--- static/js/main.js
+++ static/js/main.js
@@ -64,7 +64,7 @@ if (switcher) {
       if (tab.dataset.poster) video.poster = tab.dataset.poster;
       if (caption && tab.dataset.caption) caption.textContent = tab.dataset.caption;
       video.load();
-      video.play().catch(function () {});
+      video.play().catch(function () { });
     });
   });
 }
@@ -74,7 +74,7 @@ var lazyVideos = document.querySelectorAll('video[data-autoplay]');
 if (lazyVideos.length && 'IntersectionObserver' in window) {
   var observer = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
-      if (entry.isIntersecting) entry.target.play().catch(function () {});
+      if (entry.isIntersecting) entry.target.play().catch(function () { });
       else entry.target.pause();
     });
   }, { threshold: 0.25 });
